c6de2fe80cefac48eb8e75c793ffd29f09104f10
Juan <1213@holbertonschool.com>
2020-09-27 12:55:18 -0500

    [feature] ind + closure

--- 0x0C-Javascript_advanced/13-bind_user.js
+++ 0x0C-Javascript_advanced/13-bind_user.js
@@ -1,10 +1,15 @@
-let roomDimensions = {
-    width: 50,
-    lenght: 100,
-    getArea() {
-        return this.width * this.lenght;
-    }
+let user = {
+    hobby: 'Calligraphy',
+    favoriteSport: 'Hockey',
+    astrologicalSign: 'Aries',
+    firstName: 'Buillaume',
+    lastName: 'Ialva',
+    location: 'Telaviv',
+    occupation: 'Engineer',
+}
+function logWelcomeUser(welcomeString) {
+    console.log(welcomeString + ', ' + this.firstName + '. Your occupation is:' + this.occupation);
 }
 
-let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
-console.log(boundGetArea());
+let bindLogWelcomeUser = logWelcomeUser.bind(user);
+bindLogWelcomeUser('Welcome');
